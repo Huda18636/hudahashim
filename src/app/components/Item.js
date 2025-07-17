e164701036f4f126c2b828fbb8f11a91032a786e
huda <huda_chhotani@hotmail.com>
2025-07-17 14:58:12 +0500

    assignment 5.2

--- src/app/components/Item.js
+++ src/app/components/Item.js
@@ -1,6 +1,6 @@
 export function Item({ itemName, itemDescription, itemPrice, itemImage }) {
   return (
-    <div className="flex flex-col items-start justify-start min-h-screen py-2 pl-2 bg-blue-100">
+    <div className="flex flex-col items-start justify-start min-h-auto py-2 pl-2">
       <div className="bg-white shadow-lg rounded p-6 w-50">
         <p className="text-lg font-semibold mb-4 text-center">{itemName}</p>
         <img src={itemImage} alt={itemName} className="w-full object-cover rounded mb-4" />
--- src/app/page.js
+++ src/app/page.js
@@ -8,18 +8,32 @@ export default function Home() {
     <img src="nmlogo.jpg" alt="Icon" className="h-15 mr-2" /> {/* Image as icon */}
     Nia Mia
   </h1>
-  <div className="flex flex-col items-start justify-start min-h-screen py-2 pl-2 bg-blue-100">
-    <Item 
-      itemName="huda" 
-      itemDescription="hello hellloo helllooo" 
-      itemImage="greensuit.jpg" 
-      itemPrice="12,000" 
-    />
+<div className="bg-gray-100">
+  <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 px-4 py-8">
+    {items.map((item, index) => (
+      <Item
+        key={index}
+        itemName={item.itemName}
+        itemDescription={item.itemDescription}
+        itemImage={item.itemImage}
+        itemPrice={item.itemPrice}
+      />
+    ))}
+  </div>
   </div>
 </div>
   );
 }
 const items = [
-  Item({ itemName: "huda", itemDescription: "hello hellloo helllooo", itemImage: "greensuit.jpg", itemPrice: "12,000" }),
-  
+
+  { itemName: "Luna", itemDescription: "hello hellloo helllooo", itemImage: "https://niamiaofficial.com/cdn/shop/files/1_22a6f14d-8cee-43f0-addc-2b7b4ef62143.jpg?v=1740468965", itemPrice: "13,000" },
+  { itemName: "Elle", itemDescription: "hello hellloo helllooo", itemImage: "https://niamiaofficial.com/cdn/shop/files/2_e14afd48-e05b-4d10-857a-025fee9cd23c.jpg?v=1740467906", itemPrice: "12,500" },
+  { itemName: "Midnight", itemDescription: "hello hellloo helllooo", itemImage: "https://niamiaofficial.com/cdn/shop/files/1_3be8eb23-6239-4180-b00e-4209e7296988.jpg?v=1740468610", itemPrice: "12,000" },
+  { itemName: "Rosette", itemDescription: "hello hellloo helllooo", itemImage: "https://niamiaofficial.com/cdn/shop/files/4_20c1b162-0a5e-4425-ad1e-b8b02432d4cb.jpg?v=1740468108", itemPrice: "11,000" },
+ { itemName: "Luna2", itemDescription: "hello hellloo helllooo", itemImage: "https://niamiaofficial.com/cdn/shop/files/1_22a6f14d-8cee-43f0-addc-2b7b4ef62143.jpg?v=1740468965", itemPrice: "13,000" },
+  { itemName: "Elle2", itemDescription: "hello hellloo helllooo", itemImage: "https://niamiaofficial.com/cdn/shop/files/2_e14afd48-e05b-4d10-857a-025fee9cd23c.jpg?v=1740467906", itemPrice: "12,500" },
+  { itemName: "Midnight2", itemDescription: "hello hellloo helllooo", itemImage: "https://niamiaofficial.com/cdn/shop/files/1_3be8eb23-6239-4180-b00e-4209e7296988.jpg?v=1740468610", itemPrice: "12,000" },
+  { itemName: "Rosette2", itemDescription: "hello hellloo helllooo", itemImage: "https://niamiaofficial.com/cdn/shop/files/4_20c1b162-0a5e-4425-ad1e-b8b02432d4cb.jpg?v=1740468108", itemPrice: "11,000" },
+
+
 ]
